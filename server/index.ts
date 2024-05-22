01fb24bc94a2774b02027a4e8a2e273854546837
kenvynKwek <kenvynkwek@gmail.com>
2024-05-22 23:11:22 +0800

    Vite/React set up

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,6 @@ Bun.serve({
     // port: 8080, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
     // hostname: "mydomain.com", // defaults to "0.0.0.0"
     fetch: app.fetch
-  });
+});
 
 console.log("server running");
