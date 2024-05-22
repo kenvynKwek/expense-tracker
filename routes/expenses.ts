53aa5f7ad25852f0ecd02e04e358b004b41eceef
kenvynKwek <kenvynkwek@gmail.com>
2024-05-22 16:01:46 +0800

    Implement data validation using Zod

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,4 +1,5 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { z } from 'zod';
 
 export const expensesRoute = new Hono();
 
@@ -15,6 +16,12 @@ const fakeExpenses: Expense[] = [
     { id: 3, title: "Rent", amount: 1000 }
 ];
 
+// for validating if retrieved data is the correct data type
+const createPostSchema = z.object({
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+});
+
 // get data
 expensesRoute.get("/", (c) => {
     return c.json({ expenses: fakeExpenses});
@@ -22,7 +29,8 @@ expensesRoute.get("/", (c) => {
 
 // output the info somewhere
 expensesRoute.post("/", async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data); // check data type
     console.log({expense}); // to see output
     return c.json(expense);
 });
