6d4e98d7924363214fc1145a68dab40847f8e0bb
kenvynKwek <kenvynkwek@gmail.com>
2024-05-22 14:11:45 +0800

    Add hono backend
    Created 'app.ts' to import hono backend to use routes and logger

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
+
+app.use(logger())
+
+app.get("/test", c => {
+    return c.text("test page")
+})
 
 export default app // for Cloudflare Workers or Bun
--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import { logger } from 'hono/logger';
+import app from './app';
+
 Bun.serve({
     // port: 8080, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
     // hostname: "mydomain.com", // defaults to "0.0.0.0"
-    fetch(req) {
-      return new Response("Hello from bun server");
-    },
+    fetch: app.fetch
   });
+
 console.log("server running");
