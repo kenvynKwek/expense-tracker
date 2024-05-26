4bea9b312b7e565a9aa9fb942d486b31c6393d08
kenvynKwek <kenvynkwek@gmail.com>
2024-05-26 23:11:05 +0800

    Add shadcn card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,30 @@
 import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [totalSpent, setTotalSpent] = useState(0);
 
   return (
-    <>
-      <div className="flex flex-col bg-background max-w-xs m-auto gap-y-5">
-        <Button className="text-foreground" onClick={() => setCount((count) => count + 1)}>
-          up
-        </Button>
-        <Button variant="secondary" onClick={() => setCount((count) => count - 1)}>
-          down
-        </Button>
-        <p>
-          {count}
-        </p>
-      </div>
-    </>
-  )
+    <Card className="w-[350px] m-auto">
+    <CardHeader>
+      <CardTitle>Total Spent</CardTitle>
+      <CardDescription>The total amount you've spent</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>{totalSpent}</p>
+    </CardContent>
+    <CardFooter>
+      <p>Card Footer</p>
+    </CardFooter>
+    </Card>
+  );
 }
 
-export default App
+export default App;
