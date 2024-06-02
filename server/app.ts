20816144ca5fcfc6fb73282b42fa6bb3f269b575
kenvynKwek <kenvynkwek@gmail.com>
2024-06-02 12:26:55 +0800

    Building the app
    Running 'bun run build' in frontend directory squeezes all the necessary files down to 'frontend/dist/*'. Use serveStatic function to tell the app to GET requests from 'frontend/dist'. serveStatic_path directs any invalid routes/pages to frontend/dist/index.html.

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,15 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono();
 
 app.use(logger());
 
-app.get("/test", c => {
-    return c.text("test page")
-});
-
 app.route("/api/expenses", expensesRoute); // routes localhost:3000/api/expenses to expensesRoute (/routes/expenses.ts)
+
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app; // for Cloudflare Workers or Bun
